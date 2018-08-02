7361c75cb7a6511be810d5ba322b8eaaeb1fcf37
Gleiber Granando <gleibergranado3@gmail.com>
2018-08-02 16:57:15 -0400

    Version 2

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     var $display = $("#display");
 
     var buttonID = "";
     var expression = "";
-   
+
 
 
 
     $('.btnClick').unbind('click').click(function() {
-       
+
         buttonID = ($(this).find('span')).attr('id');
         number = ($(this).find('span').text());
         console.log(number);
-    
+
         switch (buttonID) {
 
             case "clc":
@@ -48,7 +48,7 @@ $(document).ready(function() {
 
     });
     function calc(numb) {
-      
+
         expression += parseInt(numb, 10);
         $display.html(expression);
         console.log(expression);
@@ -110,4 +110,43 @@ $(document).ready(function() {
          $display.html(expression);
         console.log("dot");
     }
+});*/
+
+let calculator = document.querySelector('#calculator')
+let display = $('#display');
+calculator.addEventListener('click', function(event){
+
+	let target = event.target;
+	let dataset = target.dataset;
+	let value = dataset.value;
+	let type = dataset.type;
+	console.log(dataset);
+
 });
+
+class calculatorClass(){
+	constructor(){
+		this.init();
+	}
+
+	init(){
+		this.acumulator = [];
+		this.operator = null;
+		this.leftOperator = 0;
+		this.rigthOperator = 0;
+		this.state = null;
+		this.strategy = null;
+	}
+
+	output(){
+		let result = 0;
+		if(this.acumulator.length > 0){
+				result = this.acumulator.join('');
+		}
+		return result;
+	}
+
+	input(){
+
+	}
+}
